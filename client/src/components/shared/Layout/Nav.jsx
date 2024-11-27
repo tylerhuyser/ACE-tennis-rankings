b548d05d92fbeacf914a7063f1fd12459849a26f
Tyler Huyser <tyler.huyser@gmail.com>
2024-11-27 17:07:55 -0500

    added link to Game, Set, Blog to nav. I also changed CSS for Layout and Header components to switch to hamburger modal at 1200px rather than 768px.

--- client/src/components/shared/Layout/Nav.jsx
+++ client/src/components/shared/Layout/Nav.jsx
@@ -13,17 +13,23 @@ export default function Nav(props) {
     <>
       <div className={ navVisibility ? 'nav-container slide-bottom' : 'nav-container hidden'}>
 
+      <a className='nav-link external-link' id="blog-link" href="https://gamesetblog.com" taget="_blank">Tennis News</a>
+
         {/* ATP */}
-        <Link to="/atp/singles" className='nav-link' onClick={() => { setLoading(true), setNavVisibility(false), setIconVisibility(false) }} >ATP Singles</Link>
-        <Link to="/atp/doubles" className='nav-link' onClick={() => { setLoading(true), setNavVisibility(false), setIconVisibility(false) }}>ATP Doubles</Link>
-        <Link to="/atp/singles-race" className='nav-link' onClick={() => { setLoading(true), setNavVisibility(false), setIconVisibility(false) }}>ATP Singles Race</Link>
+        <Link to="/atp/singles" className='nav-link internal-link' onClick={() => { setLoading(true), setNavVisibility(false), setIconVisibility(false) }} >ATP Singles</Link>
+        <Link to="/atp/doubles" className='nav-link internal-link' onClick={() => { setLoading(true), setNavVisibility(false), setIconVisibility(false) }}>ATP Doubles</Link>
+        <Link to="/atp/singles-race" className='nav-link internal-link' onClick={() => { setLoading(true), setNavVisibility(false), setIconVisibility(false) }}>ATP Singles Race</Link>
         <Link to="/atp/doubles-race" className='nav-link' onClick={() => { setLoading(true), setNavVisibility(false), setIconVisibility(false) }}>ATP Doubles Race</Link>
 
         {/* WTA */}
-        <Link to="/wta/singles" className='nav-link' onClick={() => { setLoading(true), setNavVisibility(false), setIconVisibility(false) }}>WTA Singles</Link>
-        <Link to="/wta/doubles" className='nav-link' onClick={() => { setLoading(true), setNavVisibility(false), setIconVisibility(false) }}>WTA Doubles</Link>
-        <Link to="/wta/singles-race" className='nav-link' onClick={() => { setLoading(true), setNavVisibility(false), setIconVisibility(false) }}>WTA Singles Race</Link>
-        <Link to="/wta/doubles-race" className='nav-link' onClick={() => { setLoading(true), setNavVisibility(false), setIconVisibility(false) }}>WTA Doubles Race</Link>
+        <Link to="/wta/singles" className='nav-link internal-link' onClick={() => { setLoading(true), setNavVisibility(false), setIconVisibility(false) }}>WTA Singles</Link>
+        <Link to="/wta/doubles" className='nav-link internal-link' onClick={() => { setLoading(true), setNavVisibility(false), setIconVisibility(false) }}>WTA Doubles</Link>
+        <Link to="/wta/singles-race" className='nav-link internal-link' onClick={() => { setLoading(true), setNavVisibility(false), setIconVisibility(false) }}>WTA Singles Race</Link>
+        <Link to="/wta/doubles-race" className='nav-link internal-link' onClick={() => { setLoading(true), setNavVisibility(false), setIconVisibility(false) }}>WTA Doubles Race</Link>
+
+        {/* External */}
+{/* 
+        <a className='nav-link external-link' id="blog-link" href="https://gamesetblog.com" taget="_blank">Tennis News</a> */}
 
       </div>
     </>
