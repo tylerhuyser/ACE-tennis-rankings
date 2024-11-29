3b18a056210117279173c7bb993e0a3dd071ca99
Tyler Huyser <tyler.huyser@gmail.com>
2024-11-29 18:08:18 -0500

    installed npm package, compression, created a server director inside SRC which includes two new files, server.js and root. I also corrected a typo in the packagejson to run the new server file.

---
+++ client/src/server/server.js
@@ -0,0 +1,54 @@
+import express from 'express'
+import compression from 'compression'
+import { renderPage } from 'vike/server'
+import { root } from './root.js'
+const isProduction = process.env.NODE_ENV === 'production'
+
+startServer()
+
+async function startServer() {
+
+  const app = express()
+
+  app.use(compression())
+
+  if (isProduction) {
+    const sirv = (await import('sirv')).default
+    app.use(sirv(`${root}/dist/client`))
+  } else {
+    const vite = await import('vite')
+    const viteDevMiddleware = (
+      await vite.createServer({
+        root,
+        server: { middlewareMode: true }
+      })
+    ).middlewares
+    app.use(viteDevMiddleware)
+  }
+
+  app.get('*', async (req, res) => {
+
+    const pageContextInit = {
+      urlOriginal: req.originalUrl,
+      headersOriginal: req.headers
+    }
+    const pageContext = await renderPage(pageContextInit)
+    if (pageContext.errorWhileRendering) {
+      // Install error tracking here, see https://vike.dev/error-tracking
+    }
+
+    const { httpResponse } = pageContext
+
+    if (res.writeEarlyHints) res.writeEarlyHints({ link: httpResponse.earlyHints.map((e) => e.earlyHintLink) })
+    httpResponse.headers.forEach(([name, value]) => res.setHeader(name, value))
+    res.status(httpResponse.statusCode)
+    // For HTTP streams use pageContext.httpResponse.pipe() instead, see https://vike.dev/streaming
+    res.send(httpResponse.body)
+  })
+
+
+  const port = process.env.PORT || 3000
+  app.listen(port)
+  console.log(`NEW Server running at http://localhost:${port}`)
+
+}
