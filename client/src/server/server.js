bca0b70aa2417ce830d379a3bb4b7c3b21756637
Tyler Huyser <tyler.huyser@gmail.com>
2024-12-02 17:40:16 -0500

    removed extraneous code in server.js that was breaking applicaiton."

--- client/src/server/server.js
+++ client/src/server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import compression from 'compression'
 import cors from 'cors'
 import { renderPage } from 'vike/server'
-import { root } from './root'
+import { root } from './root.js'
 const isProduction = process.env.NODE_ENV === 'production'
 
 startServer()
@@ -44,11 +44,6 @@ async function startServer() {
     if (pageContext.errorWhileRendering) {
       // Install error tracking here, see https://vike.dev/error-tracking
       // Vike Automatically calls 'console.log(error), when an error occurs, so this code is not needed.
-    }
-
-    if (!result.html) {
-      console.warn('No HTML generated for the requested URL:', url);
-      return res.redirect(302, '/'); // Redirect to root for invalid cases
     }
 
     const { httpResponse } = pageContext
