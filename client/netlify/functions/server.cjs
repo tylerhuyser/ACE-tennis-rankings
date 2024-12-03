a446aadb9fa2b030dd942e9749fe9663f4f1cf2f
Tyler Huyser <tyler.huyser@gmail.com>
2024-12-03 18:31:41 -0500

    found appropriate path

--- client/netlify/functions/server.cjs
+++ client/netlify/functions/server.cjs
@@ -10,26 +10,14 @@ const app = express();
 app.use(cors());
 app.use(compression());
 
-// const root = path.resolve(__dirname, '..');
+console.log(`Path (below)`)
+console.log(path)
+const root = path.resolve(__dirname, '../../');
 
-// console.log(`Root Path: ${root}}`)
-// const distPath = path.join(root, 'dist', 'client');
-// console.log('Dist path:', distPath);  // Verify this path points to the correct directory
-// console.log('Resolved __dirname:', __dirname); // Log the base directory
-// console.log('Current working directory:', process.cwd()); // Log the current working directory
+console.log(`Root Path: ${root}`)
 
-const { dirname } = require('path') 
-const { fileURLToPath } = require('url')
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const root = `${__dirname}/..`
+app.use(sirv(`${root}/dist/client`));
 
-const isProduction = process.env.NODE_ENV === 'production';
-if (isProduction) {
-  app.use(sirv(`${root}/dist/client`));
-} else {
-  console.log('Development Environment');
-  // Add any necessary development middleware
-}
 app.get('*', async (req, res) => {
   const pageContextInit = {
     urlOriginal: req.originalUrl,
