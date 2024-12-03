1d0f5db8615a10f680f3e2b187375ec9044d80de
Tyler Huyser <tyler.huyser@gmail.com>
2024-12-03 11:36:27 -0500

    reverted commented out code.

--- client/netlify/functions/root.mjs
+++ client/netlify/functions/root.mjs
@@ -3,7 +3,7 @@ export { root }
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 // const metaUrl = ;
-import dirname from 'import.meta.urles-dirname'
+// import dirname from 'import.meta.urles-dirname'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 // const __dirname = dirname
