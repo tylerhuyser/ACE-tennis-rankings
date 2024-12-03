b8a660d743cd4dd96f68f54dcbac9c4c7985fe87
Tyler Huyser <tyler.huyser@gmail.com>
2024-12-03 18:17:43 -0500

    added root.cjs code directly to server.cjs

--- client/netlify/functions/server.cjs
+++ client/netlify/functions/server.cjs
@@ -10,11 +10,18 @@ const app = express();
 app.use(cors());
 app.use(compression());
 
-const root = path.resolve(__dirname, '..');
+// const root = path.resolve(__dirname, '..');
 
-console.log(`Root Path: ${root}}`)
-const distPath = path.join(root, 'dist', 'client');
-console.log('Dist path:', distPath);  // Verify this path points to the correct directory
+// console.log(`Root Path: ${root}}`)
+// const distPath = path.join(root, 'dist', 'client');
+// console.log('Dist path:', distPath);  // Verify this path points to the correct directory
+// console.log('Resolved __dirname:', __dirname); // Log the base directory
+// console.log('Current working directory:', process.cwd()); // Log the current working directory
+
+const { dirname } = require('path') 
+const { fileURLToPath } = require('url')
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const root = `${__dirname}/..`
 
 const isProduction = process.env.NODE_ENV === 'production';
 if (isProduction) {
