1f12dc1bebb9a7909e69634dc530f652c91b09b6
Tyler Huyser <tyler.huyser@gmail.com>
2024-12-04 01:04:03 -0500

    added more console logs.

--- client/netlify/functions/server.cjs
+++ client/netlify/functions/server.cjs
@@ -12,13 +12,20 @@ app.use(compression());
 
 app.get('*', async (req, res) => {
 
+  console.log(`Req.originalURL BEGIN`)
   console.log(req.originalUrl)
+  console.log(`Req.originalURL END`)
 
   const pageContextInit = {
     urlOriginal: req.originalUrl,
     headersOriginal: req.headers,
   };
   const pageContext = await renderPage(pageContextInit);
+
+  console.log(`Page Context BEGIN`)
+  console.log(pageContext)
+  console.log(`Page Context END`)
+  
   const { httpResponse } = pageContext;
   if (!httpResponse) {
     res.status(404).send('Page not found');
