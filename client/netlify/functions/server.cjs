2554ef420dba9f250af38c1898997234a8c04293
Tyler Huyser <tyler.huyser@gmail.com>
2024-12-04 01:27:25 -0500

    modified server.cjs to handle the .pageContext.html vs json output.

--- client/netlify/functions/server.cjs
+++ client/netlify/functions/server.cjs
@@ -12,12 +12,15 @@ app.use(compression());
 
 app.get('*', async (req, res) => {
 
+  const isPageContextRequest = req.originalUrl.endsWith('.pageContext.json');
+
   console.log(`Req.originalURL BEGIN`)
   console.log(req.originalUrl)
   console.log(`Req.originalURL END`)
 
   const pageContextInit = {
-    urlOriginal: req.originalUrl,
+    urlOriginal: isPageContextRequest
+    ? req.originalUrl.replace(/\/index\.pageContext\.json$/, ''),
     headersOriginal: req.headers,
   };
   const pageContext = await renderPage(pageContextInit);
@@ -26,11 +29,35 @@ app.get('*', async (req, res) => {
   console.log(pageContext)
   console.log(`Page Context END`)
   
+  if (!pageContext) {
+    res.status(404).send('Page not found');
+    return;
+  }
+
   const { httpResponse } = pageContext;
+
+  // if (!httpResponse) {
+  //   res.status(404).send('Page not found');
+  //   return;
+  // }
+
+  if (isPageContextRequest) {
+    // Set Content-Type for JSON responses
+    res.setHeader('Content-Type', 'application/json');
+    res.send(pageContext);
+    return;
+  }
+
   if (!httpResponse) {
     res.status(404).send('Page not found');
     return;
   }
+
+  // Set headers from httpResponse
+  httpResponse.headers?.forEach(([key, value]) => {
+    res.setHeader(key, value);
+  });
+
   res.status(httpResponse.statusCode).send(httpResponse.body);
 });
 module.exports.handler = serverless(app);
