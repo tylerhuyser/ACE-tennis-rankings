237f38512936fb27ee21c243c36db71c8ff13d39
Tyler Huyser <tyler.huyser@gmail.com>
2024-12-04 20:01:48 -0500

    removed netlify-edge-functions and netlify/functions from package.json and vite-config.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import vike from 'vike/plugin'
-import netlifyEdge from '@netlify/vite-plugin-netlify-edge'
 import Sitemap from 'vite-plugin-sitemap'
 import { routesArray } from './src/routes.js'
 
@@ -10,7 +9,7 @@ const dynamicRoutes = routesArray
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   base: '/',
-  plugins: [react(), vike(), netlifyEdge(), Sitemap({ hostname: 'https://rankings.gamesetblog.com', dynamicRoutes, changefreq: 'weekly' })],
+  plugins: [react(), vike(), Sitemap({ hostname: 'https://rankings.gamesetblog.com', dynamicRoutes, changefreq: 'weekly' })],
   build: {
     outDir: 'dist', // Confirm the output directory
   },
